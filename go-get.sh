9ac374eaef832474858602baf1934c95c0f61367
Anton Moscal <msk@wolpike.com>
2017-12-09 06:52:29 +0300

    init

---
+++ go-get.sh
@@ -0,0 +1,5 @@
+go get github.com/kouzdra/go-analyzer
+go get github.com/kouzdra/go-gode
+go get github.com/kouzdra/go-scintilla
+go get github.com/kouzdra/go-tools
+go get github.com/kouzdra/go-livejournal
